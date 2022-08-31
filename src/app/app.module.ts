53baf08db9bd6bcfcea2616261968d5556e30e4c
colbygllhr <cjg0033@mix.wvu.edu>
2022-08-31 10:47:23 -0400

    added more navigation components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TopNavBarComponent } from './navigation/top-nav-bar/top-nav-bar.component';
 import { SecNavBarComponent } from './navigation/sec-nav-bar/sec-nav-bar.component';
+import { BottomNavBarComponent } from './navigation/bottom-nav-bar/bottom-nav-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TopNavBarComponent,
-    SecNavBarComponent
+    SecNavBarComponent,
+    BottomNavBarComponent
   ],
   imports: [
     BrowserModule,

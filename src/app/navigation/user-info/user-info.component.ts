e873562974402758170418bfec49c103a6ff5ef5
colbygllhr <cjg0033@mix.wvu.edu>
2022-10-22 10:37:03 -0400

    updated user info module (firebase)

--- src/app/navigation/user-info/user-info.component.ts
+++ src/app/navigation/user-info/user-info.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Injectable } from '@angular/core';
 import { HttpClient } from "@angular/common/http";
+import { UserInfo } from './user-info.model';
 
 @Injectable()
 @Component({
@@ -8,16 +9,30 @@ import { HttpClient } from "@angular/common/http";
   styleUrls: ['./user-info.component.css']
 })
 export class UserInfoComponent implements OnInit {
+  myInfo: UserInfo | undefined;
+
 
   constructor(private http: HttpClient) { 
 
   }
 
   ngOnInit(): void {
+    console.log("Sending get request to server...")
+    this.getUserInfo();
+    console.log("Registering showUserInfo as a subscriber...");
+    this.showUserInfo();
   }
 
   getUserInfo() {
-    return this.http.get('https://reverb-app-default-rtdb.firebaseio.com/my-info.json');
+    return this.http.get<UserInfo>('https://reverb-app-default-rtdb.firebaseio.com/my-info.json');
+  }
+
+  showUserInfo(){
+    this.getUserInfo().subscribe((data: UserInfo) => {
+      console.log(data);
+      this.myInfo = data;
+      this.myInfo = undefined;
+    })
   }
 
 }

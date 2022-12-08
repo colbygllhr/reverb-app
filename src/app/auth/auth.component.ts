294a643795916d5f5534f883c63b379974227d86
colbygllhr <cjg0033@mix.wvu.edu>
2022-12-08 18:55:59 -0500

    finished lab 10 code

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { Observable } from 'rxjs';
+import { AuthService } from './auth.service';
+import { AuthResponse } from "./authResponse";
 
 @Component({
   selector: 'app-auth',
@@ -8,15 +11,40 @@ import { NgForm } from '@angular/forms';
 })
 export class AuthComponent implements OnInit {
 
-  constructor() { }
+  public buttonClicked!:string;
+  private authObservable! : Observable<AuthResponse>;
+
+  constructor(private authService:AuthService) { 
+
+  }
 
   ngOnInit(): void {
   }
 
   public onSubmit(data: NgForm){
     console.log("Button clicked...")
-    console.log(data)
+    console.log(data.value);
 
+    if(this.buttonClicked == 'SignUp'){
+    this.authObservable = this.authService.signup(data.value.email, data.value.password);
   }
 
+  if(this.buttonClicked == 'Login'){
+    this.authObservable = this.authService.login(data.value.email, data.value.password);
+  }
+    
+    
+    this.authObservable.subscribe( 
+      (data:AuthResponse)=>{
+        console.log(data);
+      },
+      error => {
+        console.log(error.error);
+        
+      } 
+
+    ); 
+
+    data.resetForm();
+    }
 }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,17 @@
-export const environment = {
-  production: true
-};
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
+
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
@@ -9,17 +20,20 @@ import { getAnalytics } from "firebase/analytics";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: 'AIzaSyDTB9NAxFwDp36gQSq0IoSQPJCMQJ0fw3w',
-  authDomain: 'reverb-app.firebaseapp.com',
-  databaseURL: 'https://reverb-app-default-rtdb.firebaseio.com',
-  projectId: 'reverb-app',
-  storageBucket: 'reverb-app.appspot.com',
-  messagingSenderId: '491374298674',
-  appId: '1:491374298674:web:8a014806bd9bb2be0ade2b',
-  measurementId: 'G-YS9ZSS4R5P',
+export const environment = {
+ firebase:{
+   apiKey: 'AIzaSyDTB9NAxFwDp36gQSq0IoSQPJCMQJ0fw3w',
+    authDomain: 'reverb-app.firebaseapp.com',
+    databaseURL: 'https://reverb-app-default-rtdb.firebaseio.com',
+    projectId: 'reverb-app',
+    storageBucket: 'reverb-app.appspot.com',
+    messagingSenderId: '491374298674',
+    appId: '1:491374298674:web:8a014806bd9bb2be0ade2b',
+    measurementId: 'G-YS9ZSS4R5P',
+},
+production: false
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+const app = initializeApp(environment.firebase);
 const analytics = getAnalytics(app);
